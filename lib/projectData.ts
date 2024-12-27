f4b45572dcecdedf7716968842ede885d22b32b6
Satria Ardan Wicaksono <137624428+wcksnrdn@users.noreply.github.com>
2024-12-27 21:01:00 +0700

    Add files via upload

---
+++ lib/projectData.ts
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    id: 1,
+    slug: "motion-ime-fest",
+    title: "Motion Ime Fest",
+    description:
+      "Motion Ime Fest is an interactive platform celebrating the fusion of art and technology through a vibrant festival experience. It features a ticket booking system for seamless event participation and a rich gallery of photos documenting past chapters, showcasing the journey and growth of the festival.",
+    image: "/imagesproject/motionimefestfull.png",
+    tech: ["HTML5", "PHP", "Laravel", "CSS3", "Javascript"],
+    linkProject: "https://google.com",
+    repo: "https://github.com/wcksnrdn/UI-UX-Motion-Ime-",
+  },
+  {
+    id: 2,
+    slug: "phy-schools",
+    title: "PhySchools",
+    description:
+      "PhySchools is an educational platform dedicated to making physics learning accessible and engaging. With interactive lessons, practical examples, and a user-friendly interface, it bridges the gap between theory and application, empowering students to master complex physics concepts effortlessly.",
+    image: "/imagesproject/physchoolsfull.png",
+    tech: ["HTML5", "PHP", "CSS3", "MySQL", "Javascript"],
+    linkProject: "https://google.com",
+    repo: "https://github.com/wcksnrdn/PhySchools",
+  },  
+  {
+    id: 3,
+    slug: "la-vaguelette",
+    title: "La Vaguelette",
+    description:
+      "La Vaguelette is a delightful website designed to showcase and market premium handmade cookies. With an elegant design and seamless user experience, customers can explore various flavors, place orders effortlessly, and learn about the artisanal process behind each batch of cookies.",
+    image: "/imagesproject/lavaguelettefull.png",
+    tech: ["HTML5", "Javascript", "Node.js", "CSS3"],
+    linkProject: "https://google.com",
+    repo: "https://github.com/wcksnrdn/LaVaguelette",
+  },  
+  {
+    id: 4,
+    slug: "project-4",
+    title: "Project 4",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "/images/project-44.png",
+    tech: ["React", "TypeScript", "Next.js", "TailwindCSS"],
+    linkProject: "https://google.com",
+    repo: "https://github.com/aafrzl",
+  },
+]
